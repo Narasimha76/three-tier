9ca3782de843387d40fc82fb3467b1406ec986c6
Narasimha <123463333+Narasimha76@users.noreply.github.com>
2024-08-01 18:30:05 +0530

    Update db.js

--- Application-Code/backend/db.js
+++ Application-Code/backend/db.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2/promise');
 
 // Create a pool of connections with hardcoded values
 const connection = mysql.createPool({
-  host: 'localhost',    // Replace with your DB host
-  user: 'root',         // Replace with your DB user
-  password: 'Narasimha123', // Replace with your DB password
-  database: 'mydatabase', // Replace with your DB name
+  host: 'grafana-testing.cx4aeiycs6rj.ap-northeast-1.rds.amazonaws.com',// Replace with your DB host
+  port: 6638,
+  user: 'admin',         // Replace with your DB user
+  password: 'Shekhar101010', // Replace with your DB password
+  database: 'sample_user', // Replace with your DB name
   connectionLimit: 10,  // Adjust the pool size as needed
 });
 
